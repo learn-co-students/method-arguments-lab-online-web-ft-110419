98adab00cc954b8f9e3231d651326dee581e957f
Merkeresseria Coleman <kttsmllc@gmail.com>
2019-11-13 18:17:26 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
-def introduction Merkeresseria
-  puts " Hi, my name is Merkeresseria and I am learning to program in Rb."
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+def introduction_with_language(language)
+  puts "Hi my name is #{name}and I am learning to program in #{language}."
 end
